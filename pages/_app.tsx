4627947e9efb1e22faf0115ad2ca5273e6f2f039
KStonehouse2001 <khalidstonehouse12@outlook.com>
2022-07-07 12:06:32 +0100

    wrapped app in ActionsProvider and passed actions Props to provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../shared/theme'
+import { ActionsProvider } from '../shared/contexts/actionsContext'
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ActionsProvider actions={pageProps.actions || []}>
+        <Component {...pageProps} />
+      </ActionsProvider>
     </ChakraProvider>
   )
 }
